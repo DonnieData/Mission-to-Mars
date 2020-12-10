4a583556264af6f6bb2b8a9b5bd582801ae69a4b
Donnie <Donnell.Jackson@outlook.com>
2020-12-09 20:51:18 -0600

    updated flask app and scraping script

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -7,11 +7,13 @@ import pandas as pd
 executable_path = {'executable_path':'chromedriver.exe'}
 browser = Browser('chrome', **executable_path)
 
+
+# browser varaible as arguemnt needed to automate 
+def mars_news(browser):
 # Visit the mars nasa news site
-# tell splinter to  visit the qoutes to scrape site 
 url = 'https://mars.nasa.gov/news/'
 browser.visit(url)
-# Optional delay for loading the page ( to respect terms of service and site servers)
+
 # searching for specific combination of tags and attributes ex -- ul.item_list
 browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
@@ -19,21 +21,25 @@ browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 # parse all of the html of the page with beautiful soup
 html = browser.html
 news_soup = soup(html, 'html.parser')
-# "."" is used for selecting classes, pinpoints the <li /> tag with the class of slide and the <ul /> tag with a class of item_list.
-# will select last item 
-slide_elem = news_soup.select_one('ul.item_list li.slide')
 
-# look inside of slide_elem for data within div tag that hass cotent_title as class
-slide_elem.find("div", class_='content_title')
+# Add try/except for error handling
+try:
+    slide_elem = news_soup.select_one('ul.item_list li.slide')
+    # look inside of slide_elem for data within div tag that hass cotent_title as class
+    slide_elem.find("div", class_='content_title')
+    # Use the parent element to find the first `a` tag and save it as `news_title`
+    news_title = slide_elem.find("div", class_='content_title').get_text()
+    # Use the parent element to find the paragraph text
+    news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
+# handles errors that may occur froms craping      
+except AttributeError:
+    return None, None
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-news_p
+return news_title, news_p
 
+
+def featured_image(browser): 
 # Visit the webpage.
 url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 browser.visit(url)
@@ -43,8 +49,6 @@ full_image_elem = browser.find_by_id('full_image')
 full_image_elem.click()
 
 # Find the "more info" button and click 
-# button doesnt have any unique identifiers ( eg. class or id), so searching for it by text
-# checks if element is present and returns bool 
 browser.is_element_not_present_by_text('more info', wait_time=1)
 more_info_elem = browser.links.find_by_partial_text('more info')
 more_info_elem.click()
@@ -53,31 +57,33 @@ more_info_elem.click()
 html = browser.html
 img_soup = soup(html,'html.parser')
 
-# pull the most recent image via tags and not direct image link (so that we get the updated image every time and not the specified same image)
-# Find the relative image url
-#Note src link is in double qoutes("") and not single('')
+try: 
+    # Find the relative image url
+    img_url_rel = img_soup.select_one('figure.lede a img').get("src")
 
-img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-img_url_rel
+except AttributeError:
+    return None
 
-# image only uses partial link, which is its location within the site. to get full link simply append partial onto base link of site 
+# image only uses partial link, which is its location within the site. 
 base_url = 'https://www.jpl.nasa.gov'
 img_url = f'{base_url}{img_url_rel}'
-img_url
 
-# read html page with pandas
-#pandas specifically searches for and returns a list of tables found
-df = pd.read_html('https://space-facts.com/mars/')[0]
-df.head()
+return img_url
+
+def mars_facts():
+try:    
+    # read html page with pandas
+    df = pd.read_html('https://space-facts.com/mars/')[0]
+except BaseException:
+    return None
 
 # rename columns 
 df.columns=['description', 'value']
 # set description column as index 
 df.set_index('description', inplace=True)
-df
 
 # convert extracted table to html so it can be displayed in web app/webpage
-df.to_html()
+return df.to_html()
 
 # close automated browser session
 browser.quit()
--- app.py
+++ app.py
@@ -13,3 +13,23 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# default HTML page/ homepage route
+@app.route("/")
+def index():
+    #find the mars collection in database 
+   mars = mongo.db.mars.find_one()
+   # return an html template 
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   # run imported scraping funciton 
+   mars_data = scraping.scrape_all()
+   #update mars collection with scraped data
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+
+if __name__ == "__main__":
+    app.run()
